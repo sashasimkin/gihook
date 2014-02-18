f6527a6c96ab04355ed0f02b8a3507584dcda229
Sasha Simkin <sashasimkin@gmail.com>
2014-02-18 19:09:53 +0200

    Use express. Fixed bugs. Cleaned code.

--- README.md
+++ README.md
@@ -12,14 +12,12 @@ Requirements
 
 Instalation
 ===
-```
-git clone git://github.com/sashasimkin/hook-receiver.git
-```
-1. `cd hook-reciever/`
-2. `chmod 0777 logs/`
-2. Create file `{name}.json` with config object in directory `config/` (Below about its contents).
-3. run server `node app.js IP:PORT` (IP non-required, but if you filled port only the command must looks like `node app.js :PORT`). Defaults `IP="0.0.0.0";PORT=8001`.
-4. Use `http://IP:PORT/{name}` as hook url in github, gitlab, etc.
+1. `git clone https://github.com/sashasimkin/gihook.git`
+2. `cd gihook && chmod 0777 logs/`
+3. `npm install`
+4. Create file `{name}.json` with config object in directory `config/` (Below about its contents).
+5. run server `node app.js IP:PORT` (IP non-required, but if you filled port only the command must looks like `node app.js :PORT`). Defaults `IP="0.0.0.0";PORT=3000`.
+6. Use `http://IP:PORT/{name}` as hook url in github, gitlab, etc.
 
 
 Configuration:
@@ -32,3 +30,9 @@ Configuration parameters:
 * `path` - Root path for project, shell commands has been executed here
 * `commands` - Array of shell commands, which will be performed after recieve hook (Supported {payload.*} and {task.*})
 * `refs` - payload.ref to match. String or array of strings which will be substituted to ref.match()
+
+
+Notes:
+===
+* For GitHub use `Just the push event.` setting. Payload version should be `json`. Other events currently not supported.
+* Payload which received by gihook must contain `payload.ref` key
--- app.js
+++ app.js
@@ -1,112 +1,118 @@
+/**
+ * Dependencies.
+ */
+var express = require('express');
 var http = require('http');
-var querystring = require("querystring");
+var path = require('path');
+var app = express();
 var exec = require('child_process').exec;
 var logger = require('./lib/logger');
 var TaskManager = require('./lib/TaskManager');
 String.prototype.fmt = require('./lib/format');
 
+// Sutup middlewares
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+
 //IP and PORT for web-server
 var run_argv = process.argv[2] ? process.argv[2].split(':') : [];
 process.env.IP = process.env.IP || run_argv[0] || '0.0.0.0';
-process.env.PORT = process.env.PORT || run_argv[1] || 8001;
+process.env.PORT = process.env.PORT || run_argv[1] || 3000;
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.logger('dev'));
+    app.use(express.errorHandler());
+}
 
 //Runtime variables
 var cfg = {
     dir: __dirname + '/config/',
     map: {}
 };
+//Load configs
+require("./lib/loadConfigs")(cfg.dir, cfg.map);
 
-//Observe configs
-require("./lib/observeConfigs")(cfg.dir, cfg.map);
-
-var runTask = function (task) {
+var runTask = function(task) {
     task.running();
     var command = task.commands.shift();
     if (!command) {
         return task.finished();
     }
-    
     exec(command, task.options, function(err, stdout, stderr) {
-        if (stdout) logger(task.name + '.info').log('Data from "' + command + '": ' + stdout);
-        if (stderr) logger(task.name + '.error').log('Error in "' + command + '": ' + stderr);
-        
+        if (stdout) logger(task.name + '.info').log('[out]["' + command + '"]: ' + stdout);
+        if (stderr) logger(task.name + '.error').log('[Error]["' + command + '"]: ' + stderr);
         runTask(task);
     });
 };
 
 //Setup task manager
 var queue = new TaskManager();
-queue.run(function () {
+queue.run(function() {
     runTask(this);
 }, 1000);
 
-//Create Server
-http.createServer(function (request, response) {
-    logger('access').log('Request on: ' + request.url);
+app.post('/:config', function(req, res) {
     //Strip request.url, remove first slash
-    request.url = request.url.slice(1);
-    //Prevent favicon.ico requests
-    if (request.methom == 'GET' && request.url == 'favicon.ico') return request.connection.destroy();
+    var config_name = req.params.config;
+    var config = cfg.map[config_name];
 
-    if (request.method == 'POST' && cfg.map[request.url]) {
-        var body = '';
-        request.on('data', function (data) {
-            body += data;
-            if (body.length > 1e6) {
-                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-                request.connection.destroy();
-            }
-        });
+console.log(req.body);
 
-        request.on('end', function () {
-            var parsed = querystring.parse(body);
-            var payload = parsed.payload || Object.keys(parsed)[0];
-            try {
-                payload = JSON.parse(payload);
-            } catch (e) {
-                return logger(request.url + '.error').log('Malformed json. Request body: ' + body);
-            }
-            //We need object copy!
-            var task = JSON.parse(JSON.stringify(cfg.map[request.url]));
-            task.name = request.url;
-            task.options = {
-                encoding: "utf-8",
-                env: process.env,
-                cwd: task.path
-            };
-
-            if (task.refs) {
-                var refsType = typeof task.refs;
-                if (['string', 'object'].indexOf(refsType)) {
-                    if (refsType == 'string') task.refs = [task.refs];
-
-                    var suitableRef = Object.keys(task.refs).some(function (k) {
-                        return payload.ref.match(task.refs[k]);
-                    });
-                    if (!suitableRef) return logger(request.url + '.info').log('Ref does not fit. Aborting.');
-                }
-            }
-
-            if (task.commands.length) {
-                //Access to payload and task from command definition
-                task.commands.map(function(el, i) {
-                    try{
-                        return el.fmt({payload:payload, task:task});
-                    } catch(e) {
-                        logger(request.url + '.info').log('Command must be string.' + e.stack);
-                        return '';
-                    }
-                });
-                
-                queue.push(request.url, task);
-            } else {
-                return logger(request.url + '.info').log('No commands to execute.');
-            }
-        });
-
-        response.end("Task added to queue!");
-    } else {
-        response.writeHead(200);
-        response.end("There is something, you don't need know.");
+    if (!config || req.body.zen) {
+        logger(config_name + '.error').log('Invalid request for config: ' + config_name);
+        // Log invalid request
+        return;
     }
-}).listen(process.env.PORT, process.env.IP);
+    var payload = req.body;
+    //We need object copy!
+    var task = JSON.parse(JSON.stringify(config));
+    task.name = config_name;
+    task.options = {
+        encoding: "utf-8",
+        env: process.env,
+        cwd: task.path
+    };
+    if (task.refs) {
+        var refsType = typeof task.refs;
+        if (['string', 'object'].indexOf(refsType)) {
+            if (refsType == 'string') task.refs = [task.refs];
+            var suitableRef = Object.keys(task.refs).some(function(k) {
+                return payload.ref.match(task.refs[k]);
+            });
+            if (!suitableRef) return logger(config_name + '.info').log('Ref does not fit. Aborting.');
+        }
+    }
+    // Preprocess commands related to request
+    if (task.commands.length) {
+        //Access to payload and task from command definition
+        task.commands.filter(function(el, i) {
+            if (el.fmt) {
+                task.commands[i] = el.fmt({
+                    payload: payload,
+                    task: task
+                });
+                return true;
+            } else {
+                logger(config_name + '.info').log('Command must be string.' + e.stack);
+                return false;
+            }
+        });
+        queue.push(config_name, task);
+    } else {
+        return logger(config_name + '.info').log('No commands to execute.');
+    }
+
+    res.send(".");
+});
+
+app.all('*', function(req, res) {
+    res.send("There is nothing. Go ahead!");
+});
+
+
+http.createServer(app).listen(process.env.PORT, process.env.IP, function() {
+    console.log('GiHook got running on ' + process.env.IP + ':' + process.env.PORT + ' Enjoy!');
+});
