7e7ebed3b47b00c9f8994ee8832ef2e09cfcf474
Sasha Simkin <sashasimkin@gmail.com>
2014-02-22 05:02:27 +0200

    Upload to npm. Many refactor, way to use it as cli-tool, more params

--- README.md
+++ README.md
@@ -7,22 +7,43 @@ Created for prevent a routine work, such as working with database, static files,
 Requirements
 ===
 * nodejs >= v0.6.21
-* OS: Linux(tested on Ubuntu), *BSD(Not tested)
+* OS: Linux(tested on Ubuntu, Debian), *BSD(Not tested)
 
 
 Instalation
 ===
+There are two ways to install:
+
+First, preferred way by installing from npm:
+
+1. `npm install -g gihook`
+
+Than you have command `gihook` available.
+
+
+Second is cloning repo and use it as:
+
 1. `git clone https://github.com/sashasimkin/gihook.git`
 2. `cd gihook && chmod 0777 logs/`
-3. `npm install`
-4. Create file `{name}.json` with config object in directory `config/` (Below about its contents).
-5. run server `node app.js IP:PORT` (IP non-required, but if you filled port only the command must looks like `node app.js :PORT`). Defaults `IP="0.0.0.0";PORT=3000`.
-6. Use `http://IP:PORT/{name}` as hook url in github, gitlab, etc.
+
+Than use it as `forever app.js --params` or `npm link .` than you have command `gihook` available :-)
+
+
+Usage
+===
+1. Create file `{name}.json` with config object in directory `config/`(Or any other that you pass to script) (Below about its contents).
+2. Start it as gihook 0.0.0.0:3000 --config path/to/configs/dir [--config path/to/config/file.json -c /some/other/file.json] ...
+3. Use `http://IP:PORT/{name}` as hook url in github, gitlab, etc.
+
+If you want use it with forever ot anything such - start it as `forever \`which gihook\` [options ...]`
+Write 'gihook -h' for more info.
 
 
 Configuration:
 ===
-It is a json file with json object inside in directory config/. This configuration used every time when hook has been recieved.
+Any endpoint has separated configuration which runs on each push.
+It is file with json object inside in directory config/(By default, if you install from repo) or directories and files you specify.
+Any file you pass to gihook must have `.json` file extension.
 
 
 Configuration parameters:
@@ -34,5 +55,12 @@ Configuration parameters:
 
 Notes:
 ===
+* Don't duplicate config names, in this case will be used config that loaded later
 * For GitHub use `Just the push event.` setting. Payload version should be `json`. Other events currently not supported.
 * Payload which received by gihook must contain `payload.ref` key
+
+
+TODO:
+===
+* Write tests
+* Look on this as alternative to own logger https://github.com/nomiddlename/log4js-node or this https://github.com/trentm/node-bunyan or https://github.com/flatiron/winston
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /**
  * Dependencies.
  */
@@ -6,44 +7,80 @@ var http = require('http');
 var path = require('path');
 var app = express();
 var exec = require('child_process').exec;
-var logger = require('./lib/logger');
+var Logger = require('./lib/Logger');
 var TaskManager = require('./lib/TaskManager');
 String.prototype.fmt = require('./lib/format');
 
-// Sutup middlewares
+// Setup express-related settings
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 
+// Configure arguments
+var opts = require("nomnom")
+    .option('config', {
+        list: true,
+        abbr: 'c',
+        default: ['config'],
+        help: 'Directories which contains JSON files or paths to JSON-files'
+    }).option('address', {
+        position: 0,
+        help: 'Specify address bind to. E.g. 127.0.0.1:3005 or 0.0.0.0 or only port 3005 address by default 0.0.0.0',
+        default: '0.0.0.0:3000'
+    }).option('logs', {
+        abbr: 'l',
+        help: 'Set directory for logs, if not set - fallback to logging in console',
+        default: false
+    }).option('debug', {
+        abbr: 'd',
+        help: 'Enable logging to console',
+        flag: true,
+        default: false
+    }).parse();
+
+// Set logs directory
+if(opts.logs) {
+    Logger.directory = opts.logs;
+}
+
 //IP and PORT for web-server
-var run_argv = process.argv[2] ? process.argv[2].split(':') : [];
-process.env.IP = process.env.IP || run_argv[0] || '0.0.0.0';
-process.env.PORT = process.env.PORT || run_argv[1] || 3000;
+(function() {
+    // FixMe: I don't like this, but what to do
+    var address = opts.address ? String(opts.address).split(':') : [];
+
+    if(address.length == 1) {
+        if(!isNaN(parseFloat(address[0])) && isFinite(address[0])) {
+            address = ['0.0.0.0', parseInt(address[0])];
+        } else {
+            address = [address[0], 3000];
+        }
+    }
+    process.env.IP = process.env.IP || address[0];
+    process.env.PORT = process.env.PORT || address[1];
+})();
+
 
 // development only
-if ('development' == app.get('env')) {
+if (opts.debug) {
     app.use(express.logger('dev'));
     app.use(express.errorHandler());
 }
 
-//Runtime variables
-var cfg = {
-    dir: __dirname + '/config/',
-    map: {}
-};
-//Load configs
-require("./lib/loadConfigs")(cfg.dir, cfg.map);
+// Load configs to persistent for process object
+var configs = require("./lib/loadConfigs")(opts.config);
 
 var runTask = function(task) {
+    var logger = new Logger(task.name);
+
     task.running();
     var command = task.commands.shift();
     if (!command) {
         return task.finished();
     }
     exec(command, task.options, function(err, stdout, stderr) {
-        if (stdout) logger(task.name + '.info').log('[out]["' + command + '"]: ' + stdout);
-        if (stderr) logger(task.name + '.error').log('[Error]["' + command + '"]: ' + stderr);
+        if (stdout) logger.info('[out]["' + command + '"]: ' + stdout);
+        if (stderr) logger.error('[Error]["' + command + '"]: ' + stderr);
         runTask(task);
     });
 };
@@ -54,16 +91,19 @@ queue.run(function() {
     runTask(this);
 }, 1000);
 
+// Main route that responsible for each request
 app.post('/:config', function(req, res) {
     //Strip request.url, remove first slash
     var config_name = req.params.config;
-    var config = cfg.map[config_name];
+    var config = configs[config_name];
+    var logger = new Logger(config_name);
 
     if (!config || req.body.zen) {
-        logger(config_name + '.error').log('Invalid request for config: ' + config_name);
+        logger.error('Invalid request for config: ' + config_name);
         // Log invalid request
         return;
     }
+
     var payload = req.body;
     //We need object copy!
     var task = JSON.parse(JSON.stringify(config));
@@ -80,27 +120,31 @@ app.post('/:config', function(req, res) {
             var suitableRef = Object.keys(task.refs).some(function(k) {
                 return payload.ref.match(task.refs[k]);
             });
-            if (!suitableRef) return logger(config_name + '.info').log('Ref does not fit. Aborting.');
+            if (!suitableRef) return logger.info('Ref does not fit. Aborting.');
         }
     }
-    // Preprocess commands related to request
+
+    // Pre-process commands related to request
     if (task.commands.length) {
         //Access to payload and task from command definition
-        task.commands.filter(function(el, i) {
-            if (el.fmt) {
-                task.commands[i] = el.fmt({
+        task.commands.filter(function(command, i) {
+            if (command.fmt) {
+                task.commands[i] = command.fmt({
                     payload: payload,
                     task: task
                 });
                 return true;
             } else {
-                logger(config_name + '.info').log('Command must be string.' + e.stack);
-                return false;
+                return logger.info('Unexpected command "{c}". Command must be a string.'.fmt({c: command}));
             }
         });
+
+        // And deal with it for this time. Now let the queue sort it
         queue.push(config_name, task);
+
+        logger.info('Got valid request on: ' + config_name);
     } else {
-        return logger(config_name + '.info').log('No commands to execute.');
+        return logger.info('No commands to execute.');
     }
 
     res.send(".");
@@ -108,6 +152,16 @@ app.post('/:config', function(req, res) {
 
 app.all('*', function(req, res) {
     res.send("There is nothing. Go ahead!");
+});
+
+process.on('uncaughtException', function(err) {
+    if(err.errno === 'EADDRINUSE') {
+        console.log('Can\'t start server. Address {a} already in use.'.fmt({a: opts.address}));
+    } else {
+        console.log(err);
+        console.log('Please leave traceback on https://github.com/sashasimkin/gihook/issues if you can.');
+    }
+    process.exit(1);
 });
 
 
